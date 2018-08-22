6e06a4a4ada13939203d7ac9958a152ddfee41a7
Sahana Vishwanatha <sahanachanu@gmail.com>
2018-08-22 00:44:35 -0400

    Changes made to NewDelhi and pictures page of the app

--- cities.js
+++ cities.js
@@ -4,32 +4,13 @@ if(window.location.href == "WEBPAGE.html")
     document.getElementById("MenuBarHome").classList.add("MenuBarActive");
 }
 
-var allSpan = document.getElementsByTagName('SPAN');
-for(var i = 0; i < allSpan.length; i++){
-    allSpan[i].onclick=function(){
-        if(this.parentNode){
 
-            //console.log(this.childNodes[0].nextSibling.nodeValue);
-            var childList = this.parentNode.getElementsByTagName('div');
-           
-            for(var j = 0; j< childList.length;j++){
-                var ChildId = childList[j].id;
-                var currentState = childList[j].style.display;
-                if(currentState=="none")
-                    {
-                   
-                                childList[j].style.display="block";
-                                this.childNodes[0].src='assets/images/minus.png';
-                                this.childNodes[0].alt='Collapse';   
-                       
-                      }
-                else
-                    {
-                                childList[j].style.display="none";
-                                   this.childNodes[0].src='assets/images/add.png';
-                                this.childNodes[0].alt='Expand';
-                    }
-                }
-        }
-    }
+function TransportInfo(){
+    document.getElementById("TransportDashboard").style.display = "block";
+    document.getElementById("ClimateDashboard").style.display = "none";
+}
+
+function ClimateInfo(){
+    document.getElementById("TransportDashboard").style.display = "none";
+    document.getElementById("ClimateDashboard").style.display = "block";
 }
