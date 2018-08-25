48ba87477e904448294ca57f9944146772b429e1
Sahana Vishwanatha <sahanachanu@gmail.com>
2018-08-25 18:20:50 -0400

    Changes made to places-must visit page of New Delhi

--- cities.js
+++ cities.js
@@ -4,13 +4,20 @@ if(window.location.href == "WEBPAGE.html")
     document.getElementById("MenuBarHome").classList.add("MenuBarActive");
 }
 
+function PlacesInfo(){
+    document.getElementById("PlacesDashBoard").style.display = "block";
+    document.getElementById("TransportDashboard").style.display = "none";
+    document.getElementById("ClimateDashboard").style.display = "none";
+}
 
 function TransportInfo(){
+    document.getElementById("PlacesDashBoard").style.display = "none";
     document.getElementById("TransportDashboard").style.display = "block";
     document.getElementById("ClimateDashboard").style.display = "none";
 }
 
 function ClimateInfo(){
+    document.getElementById("PlacesDashBoard").style.display = "none";
     document.getElementById("TransportDashboard").style.display = "none";
     document.getElementById("ClimateDashboard").style.display = "block";
 }
